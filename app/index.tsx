b0ffb42f26726091183f2ac3e0e95fdbb7ea6cca
RadekKusiak71 <radoslawkusiak82@gmail.com>
2025-11-20 19:30:54 +0100

    Welcome screen with button-link component and tests

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import {SafeAreaView} from 'react-native-safe-area-context';
+import ButtonLink from "@/components/button-link";
+import {theme} from "@/constants/theme";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const Index = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <View style={styles.welcomeHeader}>
+                <Text style={styles.welcomeHeaderTitle}>Welcome To Cinemax</Text>
+                <Text style={styles.welcomeHeaderSubText}>Select cinema, find your movie and book your tickets</Text>
+            </View>
+
+            <View style={styles.welcomeButtonsContainer}>
+                <View style={styles.welcomeUpperButtonsContainer}>
+                    <ButtonLink text='Sign Up' href='/' style={styles.buttonFlex} />
+                    <ButtonLink text='Sign In' href='/' style={[styles.buttonFlex, styles.buttonSpacing]} />
+                </View>
+                <ButtonLink text='Continue as Guest' href='/' variant='secondary'/>
+            </View>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        padding: 20,
+        backgroundColor: theme.colors.background,
+    },
+    welcomeHeader: {
+        marginTop: 40,
+        gap: 4
+    },
+    welcomeHeaderTitle: {
+        textAlign: 'center',
+        fontSize: 28,
+        fontWeight: 'bold',
+    },
+    welcomeHeaderSubText: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: theme.colors.textMuted,
+    },
+    welcomeButtonsContainer: {
+        gap: 12,
+    },
+    welcomeUpperButtonsContainer: {
+        flexDirection: 'row',
+        width: '100%',
+    },
+    buttonFlex: {
+        flex: 1,
+    },
+    buttonSpacing: {
+        marginLeft: 10,
+    }
+})
+
+export default Index;
